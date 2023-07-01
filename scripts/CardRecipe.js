18682b49cedf18b1d503689b54c6f497bf8da779
benji-devw <ben.nav@pm.me>
2023-07-01 17:52:28 +0200

    feature/ingredients_filter

---
+++ scripts/CardRecipe.js
@@ -0,0 +1,29 @@
+/**
+ * @description Construct card html
+ * @param {object} data - Card recipe
+ **********************************/
+export default function CardRecipe(data) {
+    return `
+        <div class="card position-relative">
+            <span class="time position-absolute rounded-5">${data.time}min</span>
+            <img src="../assets/images/${data.image}" class="card-img-top" alt="${data.name}">
+            <div class="card-body">
+                <h3 class="card-title fw-bold my-4">${data.name}</h3>
+                <h4>RECETTE</h4>
+                <p class="card-text">${data.description}</p>
+            </div>
+            
+            <div class="card-body">
+                <h4>INGREDIENS</h4>
+                <ul class="row row-cols-2 p-0">
+                  ${data.ingredients.map(item => `
+                    <li class="col my-2"> 
+                      ${item.ingredient} 
+                      <p>${item.quantity ? item.quantity : '-'} ${item.unit ? item.unit : ''}</p>
+                    </li>
+                  `).join('')}
+                </ul>
+            </div>
+        </div>
+    `
+}
--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,22 @@
-import recipes from "/data/recipes.js"
+import recipes from "../data/recipes.js"
+import CardRecipe from "./CardRecipe.js";
+
+
+
 
 
 /**
- * @description Construct card html
- * @param {object} data - Card recipe
+ * @description Filter header bar
+ * @param {string} searchValue - Search term
+ * @param {array} data - All Data
+ * @returns {array} - Data filtered
  **********************************/
-function CardRecipe(data) {
-    return `
-        <div class="card position-relative">
-            <span class="time position-absolute rounded-5">${data.time}min</span>
-            <img src="../assets/images/${data.image}" class="card-img-top" alt="${data.name}">
-            <div class="card-body">
-                <h3 class="card-title fw-bold my-4">${data.name}</h3>
-                <h4>RECETTE</h4>
-                <p class="card-text">${data.description}</p>
-            </div>
-            
-            <div class="card-body">
-                <h4>INGREDIENS</h4>
-                <ul class="row row-cols-2 p-0">
-                  ${data.ingredients.map(item => `
-                    <li class="col my-2"> 
-                      ${item.ingredient} 
-                      <p>${item.quantity ? item.quantity : '-'} ${item.unit ? item.unit : ''}</p>
-                    </li>
-                  `).join('')}
-                </ul>
-            </div>
-        </div>
-    `
-}
-
-
-function filterRecipes(searchValue, data) {
-    // Convert the search value to lowercase for case-insensitive matching
+function filterBarRecipes(searchValue, data) {
+    // Convert the search value to lowercase for matching
     const searchTerm = searchValue.toLowerCase();
 
     // Filter the data based on the search term
-    const filteredData = data.filter(recipe => {
-        // Perform the matching on the recipe property
+    return data.filter(recipe => {
         const recipeDescription = recipe.description.toLowerCase();
         const recipeName = recipe.name.toLowerCase();
         const recipeAppliance = recipe.appliance.toLowerCase()
@@ -49,8 +27,32 @@ function filterRecipes(searchValue, data) {
             recipeAppliance.includes(searchTerm)
         );
     });
-    return filteredData;
 }
+
+
+function renderIngredients(recipes) {
+    const ingredientsFilter = document.querySelector('.ingredients-filter');
+    ingredientsFilter.innerHTML = '';
+    let ingredientsArray = [];
+
+    recipes.forEach(recipe => {
+        const ingredientLower = recipe.ingredients.map(e => e.ingredient.toLowerCase());
+
+        ingredientsArray = ingredientsArray.concat(ingredientLower);
+        ingredientsArray = ingredientsArray.filter((item, index) => {
+            return ingredientsArray.indexOf(item) === index;
+        });
+    })
+
+    ingredientsArray.forEach(ingredient => {
+        const liElement = document.createElement('li');
+        liElement.textContent = ingredient;
+        ingredientsFilter.appendChild(liElement);
+    });
+    console.log(ingredientsArray)
+}
+
+
 
 
 /**
@@ -61,13 +63,13 @@ function App() {
     let data = recipes
 
     const galleryElement = document.querySelector('#gallery_section .gallery');
-    const searchInput = document.querySelector('.search-bar .search')
-    const searchSubmit = document.querySelector('.search-bar button')
+    const searchInput = document.querySelector('.search-bar .search');
+    // const searchSubmit = document.querySelector('.search-bar button')
 
 
     function renderRecipes(recipes) {
         document.querySelector('.filters-count-result span').innerHTML = recipes.length
-        console.log(recipes)
+
         recipes.forEach(recipe => {
             const cardHTML = CardRecipe(recipe);
             const cardElement = document.createElement('article');
@@ -75,13 +77,16 @@ function App() {
             cardElement.innerHTML = cardHTML;
 
             galleryElement.appendChild(cardElement);
+        // renderIngredients(recipe)
         });
+        renderIngredients(recipes)
+
     }
     renderRecipes(data)
 
     searchInput.addEventListener('input', (event) => {
         if (event.target.value.length > 2) {
-            const filteredData = filterRecipes(event.target.value, data);
+            const filteredData = filterBarRecipes(event.target.value, data);
             galleryElement.innerHTML = '';
             renderRecipes(filteredData)
         } else renderRecipes(data)
